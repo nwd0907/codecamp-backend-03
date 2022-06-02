4acdc619398744069f6fad5b49c158e827687d2a
nwd0907 <nwd0907@newbizstart.com>
2022-06-02 18:11:38 +0900

    Update relation

---
+++ class/18-03-typeorm-crud-one-to-one/src/apis/boards/boards.resolver.ts
@@ -0,0 +1,37 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { BoardService } from './boards.service';
+import { Board } from './entities/board.entity';
+import { CreateBoardInput } from './dto/createBoard.input';
+
+@Resolver()
+export class BoardResolver {
+  constructor(private readonly boardService: BoardService) {}
+
+  // @Query(() => String)
+  // getHello() {
+  //   return this.boardService.aaa();
+  // }
+
+  @Query(() => [Board])
+  fetchBoards() {
+    return this.boardService.findAll();
+  }
+
+  @Mutation(() => String)
+  createBoard(
+    @Args({ name: 'writer', nullable: true }) writer: string,
+    @Args('title') title: string,
+    @Args('contents') contents: string,
+    @Args('createBoardInput') createBoardInput: CreateBoardInput,
+  ) {
+    // console.log(args);
+
+    console.log(writer);
+    console.log(title);
+    console.log(contents);
+
+    console.log(createBoardInput);
+
+    return this.boardService.create();
+  }
+}
