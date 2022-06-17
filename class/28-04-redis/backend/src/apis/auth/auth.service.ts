f0c14f19bb1308281f96b22199f88701e25b3e3c
nwd0907 <nwd0907@newbizstart.com>
2022-06-17 18:28:04 +0900

    Update elk

---
+++ class/28-04-redis/backend/src/apis/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService, //
+  ) {}
+
+  setRefreshToken({ user, res }) {
+    const refreshToken = this.jwtService.sign(
+      { email: user.email, sub: user.id },
+      { secret: 'myRefreshKey', expiresIn: '2w' },
+    );
+
+    // 개발환경
+    res.setHeader('Set-Cookie', `refreshToken=${refreshToken}; path=/;`); // path 설정 반드시 필요!! (소셜로그인에서!!)
+
+    // 배포환경
+    // res.setHeader('Access-Control-Allow-Origin', 'https://myfrontsite.com')
+    // res.setHeader(
+    //   'Set-Cookie',
+    //   `refreshToken=${refreshToken}; path=/; domain=.mybacksite.com; SameSite=None; Secure; httpOnly;`
+    // )
+  }
+
+  getAccessToken({ user }) {
+    return this.jwtService.sign(
+      { email: user.email, sub: user.id },
+      { secret: 'myAccessKey', expiresIn: '1h' },
+    );
+  }
+}
