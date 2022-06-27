26f30fd2bc65a41e56fce5546d7f5b28cee832c4
nwd0907 <nwd0907@newbizstart.com>
2022-06-27 16:08:06 +0900

    Update cloudSQL

--- class/deploy-with-docker/backend/src/app.module.ts
+++ class/deploy-with-docker/backend/src/app.module.ts
@@ -28,11 +28,11 @@ import { UserModule } from './apis/users/user.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'my-database',
+      host: '10.51.48.4',
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'mydocker03',
+      database: 'myserver03',
       entities: [__dirname + '/apis/**/*.entity.*'],
       synchronize: true,
       logging: true,
